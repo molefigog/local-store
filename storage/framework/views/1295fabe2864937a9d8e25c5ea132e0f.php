b4f053f7489a528996748a2dd88b1e4dc8464af3
Cyber Panel <admin@gw-ent.co.za>
2024-03-02 09:45:14 +0000

    Merge remote changes into main

---
+++ storage/framework/views/1295fabe2864937a9d8e25c5ea132e0f.php
@@ -0,0 +1,87 @@
+<?php echo $__env->make('front.components.head', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('front.components.menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+<div class="container">
+
+    <?php echo $__env->yieldContent('content'); ?>
+
+    <br>
+   
+    
+<div class="card">
+            <h5 class="card-header text-center">Most Downloaded Songs</h5>
+            <div class="table-responsive text-nowrap">
+                <table class="table table-sm table-dark">
+                    <thead>
+                        <tr>
+                            <th>Art</th>
+                            <th>Title</th>
+                            <th>Artist</th>
+                            <th><i class="icon-download"></i></th>
+                            
+                            <th>Buy</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                        <?php $__currentLoopData = $downloads; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $song): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            
+                    
+                            <tr>
+                                <td><img src="<?php echo e($song->image ? \Storage::url($song->image) : ''); ?>"
+                                    alt="Avatar" width="40" height="40"></td>
+                                <td><?php echo e($song->title ?? '-'); ?></td>        
+                                <td><?php echo e($song->artist ?? '-'); ?></td> 
+                                <td><?php echo e($song->downloads ?? '-'); ?></td>
+                                <td>
+                                   
+                                    <a href="<?php echo e(route('msingle.slug', ['slug' => urlencode($song->slug)])); ?>"
+                                        class="btn buy-button btn-sm">Buy R<?php echo e($song->amount ?? '-'); ?></a> 
+                                        
+                                </td>
+                            </tr>
+                       
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+   
+
+
+    <?php echo $__env->make('layouts.modal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</div>
+<?php echo $__env->make('front.components.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+<!--Main js file Style-->
+<?php echo $__env->make('front.components.js', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const songElements = document.querySelectorAll(".ms_weekly_box");
+
+        songElements.forEach(song => {
+            song.addEventListener("click", function() {
+                const title = this.getAttribute("data-title");
+                const artist = this.getAttribute("data-artist");
+                const image = this.getAttribute("data-image");
+                const url = this.getAttribute("data-url");
+
+                updateOGMetaTags(title, artist, image, url);
+            });
+        });
+
+        function updateOGMetaTags(title, artist, image, url) {
+            document.querySelector('meta[property="og:title"]').setAttribute("content", title);
+            document.querySelector('meta[property="og:description"]').setAttribute("content", artist);
+            document.querySelector('meta[property="og:image"]').setAttribute("content", image);
+            document.querySelector('meta[property="og:url"]').setAttribute("content", url);
+        }
+    });
+</script>
+</body>
+
+</html>
+<?php /**PATH /home/gw-ent.co.za/public_html/resources/views/welcome.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/212ac19030705782b468a4e208609534.php
@@ -0,0 +1,17 @@
+<?php echo $__env->yieldContent('audio'); ?>
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div class="col-md-4 d-flex align-items-center">
+              <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                <i class="icon-library_music"> </i> </a>&nbsp;&nbsp;&nbsp;
+              <span class="mb-3 mb-md-0 text-primary">© 
+                <script>
+                  document.write(new Date().getFullYear());
+                </script> GENIUS WORKS ENT</span>
+            </div>
+        
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+              <li class="ms-3"><a class="text-primary" href="#"><i class="icon-facebook" width="24" height="24"></i></a></li>&nbsp;&nbsp;
+              <li class="ms-3"><a class="text-primary" href="#"><i class="icon-whatsapp" width="24" height="24"></i></a></li>&nbsp;&nbsp;
+              <li class="ms-3"><a class="text-primary" href="#"><i class="icon-twitter" width="24" height="24"></i></a></li>&nbsp;&nbsp;
+            </ul>
+          </footer><?php /**PATH /home/gw-ent.co.za/public_html/resources/views/front/components/footer.blade.php ENDPATH**/ ?>
